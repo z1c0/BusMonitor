9a4d14922a23af0cbb998d0b431f5b76710ef65f
Wolfgang Ziegler <wolfgang.ziegler@dynatrace.com>
2022-02-01 13:45:59 +0100

    Fix oudated packages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var lessMiddleware = require('less-middleware');
 
+console.log("http://localhost:3000");
 var app = express();
 
 // view engine setup
